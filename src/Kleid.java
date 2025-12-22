034fa668b74f51ae32feefaba9f09c26be1d8eb9
Levin Rüßmann <Levin13c@gmail.com>
2025-12-22 23:47:30 +0100

    kleid als klasse

--- src/Kleid.java
+++ src/Kleid.java
@@ -1,4 +1,31 @@
 public class Kleid extends Kleidungsstueck
 {
+    private final String AuschnitArt;
+    private final String Schnitt;
+    private final int LangeInCM;
+    private final boolean HatAermel;
 
+    public Kleid(String name, String[] farbe, String groesse, String[] material, String marke, String anlass, String wetter, String bildDateiPfad, String auschnitArt, String schnitt, int langeInCM, boolean hatAermel) {
+        super(name, farbe, groesse, material, marke, anlass, wetter, bildDateiPfad);
+        AuschnitArt = auschnitArt;
+        Schnitt = schnitt;
+        LangeInCM = langeInCM;
+        HatAermel = hatAermel;
+    }
+
+    public String getAuschnitArt() {
+        return AuschnitArt;
+    }
+
+    public String getSchnitt() {
+        return Schnitt;
+    }
+
+    public int getLangeInCM() {
+        return LangeInCM;
+    }
+
+    public boolean isHatAermel() {
+        return HatAermel;
+    }
 }

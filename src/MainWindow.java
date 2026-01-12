64db2236524ebdb2596b952d9a50bb34158095c4
Levin Rüßmann <Levin13c@gmail.com>
2026-01-12 08:08:43 +0100

    javafx bibotheke

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -11,23 +13,25 @@ public class MainWindow extends Application {
         launch(args);
     }
 
+    // Hier wird das layout bestimmt
+    private Parent createContent() {
+        return new StackPane(new Text("Hello World"));
+
+    }
+
     @Override
     public void start(Stage stage) {
+        // Durch creatContent kann man start methide stander maessig verwenden
+        Scene scene = new Scene(createContent());
+        // Ab hier wird größe titel und icon festgelegt
         stage.setTitle("StyleFlow");
-        Group root = new Group();
-        Scene scene = new Scene(root);
-
-        // Ab hier ist jedes Window gleich
         Image icon = new Image("icon.png");
         stage.getIcons().add(icon);
         stage.setHeight(500);
         stage.setWidth(800);
-        stage.setResizable(false);
-        //Scene wird zu stage hinzu gefügt
+        //Scene wird zu stage hinzu gefuegt
         stage.setScene(scene);
-        //Stage wird sichtbar immer am ende
+        //Stage wird sichtbar immer am Ende!
         stage.show();
     }
-
-
 }

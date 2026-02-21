e36b3b44d69d91ebe686056b305412c23adaf1c7
Levin Rüßmann <Levin13c@gmail.com>
2026-02-21 12:52:15 +0100

    outift verbesserung

--- src/Outfit/Outft.java
+++ src/Outfit/Outft.java
@@ -1,10 +1,61 @@
 package Outfit;
 
-import KleidungsKlassen.Kleidungsstueck;
-
-import java.util.ArrayList;
+import KleidungsKlassen.*;
 
 public class Outft {
-    private ArrayList<Kleidungsstueck> OutfitTeile;
-    private int NutzerId;
+    private Kopfbedeckung kopfbedeckung;
+    private Oberteil oberteil;
+    private Unterteil unterteil;
+    private Schuhe schuhe;
+    private final int id;
+    private static int gesamtAnzahl;
+
+    public Outft(Schuhe schuhe, Unterteil unterteil, Oberteil oberteil, Kopfbedeckung kopfbedeckung) {
+        this.schuhe = schuhe;
+        this.unterteil = unterteil;
+        this.oberteil = oberteil;
+        this.kopfbedeckung = kopfbedeckung;
+        gesamtAnzahl += 1;
+        id = getGesamtAnzahl();
+    }
+
+    public static int getGesamtAnzahl() {
+        return gesamtAnzahl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Kopfbedeckung getKopfbedeckung() {
+        return kopfbedeckung;
+    }
+
+    public void setKopfbedeckung(Kopfbedeckung kopfbedeckung) {
+        this.kopfbedeckung = kopfbedeckung;
+    }
+
+    public Oberteil getOberteil() {
+        return oberteil;
+    }
+
+    public void setOberteil(Oberteil oberteil) {
+        this.oberteil = oberteil;
+    }
+
+    public Unterteil getUnterteil() {
+        return unterteil;
+    }
+
+    public void setUnterteil(Unterteil unterteil) {
+        this.unterteil = unterteil;
+    }
+
+    public Schuhe getSchuhe() {
+        return schuhe;
+    }
+
+    public void setSchuhe(Schuhe schuhe) {
+        this.schuhe = schuhe;
+    }
 }

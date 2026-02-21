e30fd4c663abaad11cef99183051a9dbc42ce841
Levin Rüßmann <Levin13c@gmail.com>
2026-02-21 16:31:59 +0100

    outfit reader

--- src/JSONHandling/JSONReaderOutfit.java
+++ src/JSONHandling/JSONReaderOutfit.java
@@ -1,5 +1,27 @@
 package JSONHandling;
 
-public class JSONReaderOutfit {
 
+import KleidungsKlassen.KleidungsContainer;
+import Outfit.Outfit;
+
+import java.util.ArrayList;
+
+import static JSONHandling.JSONReaderHelper.LesenSafe;
+public class JSONReaderOutfit {
+    public ArrayList<Outfit> OutfitReader(KleidungsContainer kleidungsContainer) {
+        String[][] OutfitList = LesenSafe("Outfit");
+        ArrayList<Outfit> OutfitArrayList = new ArrayList<Outfit>();
+        if(OutfitList.length !=0)
+
+        {
+            for (String[] OutfitS : OutfitList) {
+                OutfitArrayList.add(new Outfit(OutfitS,kleidungsContainer));
+            }
+            return OutfitArrayList;
+        }else
+
+        {
+            return null;
+        }
+    }
 }
